f23e6a3c95afa5e8f1e3a5ba4374e4a7c7b4dd51
oleksandr markov <ol.markov1999@gmail.com>
2022-04-17 18:45:06 +0300

    menu

---
+++ main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+# include <math.h>
+using namespace std;
+int main(int argc, char** argv) {
+	cout<< "1 - Rounding functions"<< endl;
+	cout<< "2 - PI value"<< endl;
+	cout<< "3 - Powers, Roots, Logarithms"<< endl;
+	cout<< "4 - Min/Max"<< endl;
+	cout<< "5 - Trigonometric functions"<< endl;
+	cout<< "6 - Exit"<< endl;
+	system("pause");
+	return 0;
+}
